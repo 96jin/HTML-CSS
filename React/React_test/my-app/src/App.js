9fbffe5581ced729de0f09b5a8f27077fe1f47de
Jinhyeok <gjwlsgur123@gmail.com>
2022-09-26 16:00:41 +0900

    react

--- React/React_test/my-app/src/App.js
+++ React/React_test/my-app/src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
+import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent2'
 import Extraction from './components/2-4.Props/Extraction/Extraction'
 import Composition from './components/2-4.Props/Composition'
 import ClassComponent from './components/2-5.State/ClassComponent'
-import FunctionalComponent from './components/2-5.State/FunctionalComponent';
+import FunctionalComponent from './components/2-5.State/FunctionalComponent'
 
 function App() {
   return (
     <div className="App">
+      <ClassComponent2 />
       <FunctionalComponent/>
       <ClassComponent />
       <Extraction/>
