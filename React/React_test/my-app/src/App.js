4d6e3b9de0d686eb2d50d3c3ced56d7beb63b64d
Jinhyeok <gjwlsgur123@gmail.com>
2022-09-27 16:59:17 +0900

    react

--- React/React_test/my-app/src/App.js
+++ React/React_test/my-app/src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
-import Event from './components/2-7.Event/Event'
+import Condition from './components/2-8.ConditionalRendering/Condition'
+// import Event from './components/2-7.Event/Event'
 // import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent2'
 // import Extraction from './components/2-4.Props/Extraction/Extraction'
 // import Composition from './components/2-4.Props/Composition'
@@ -10,7 +11,8 @@ import Event from './components/2-7.Event/Event'
 function App() {
   return (
     <div className="App">
-      <Event/>
+      <Condition/>
+      {/* <Event/> */}
       {/* <ClassComponent2 />
       <FunctionalComponent/>
       <ClassComponent />
