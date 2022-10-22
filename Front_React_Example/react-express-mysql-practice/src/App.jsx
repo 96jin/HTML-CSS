6ac5858ae0de80e41d1a9b763b4f3fc57fad61ee
Jinhyeok <gjwlsgur123@gmail.com>
2022-10-22 17:42:20 +0900

    react express mysql - practice

--- Front_React_Example/react-express-mysql-practice/src/App.jsx
+++ Front_React_Example/react-express-mysql-practice/src/App.jsx
@@ -1,23 +1,69 @@
 import './App.css';
 import axios from 'axios'
+import { useState } from 'react';
+
 // axios 모듈에서 axios 함수를 불러온다. ($.ajax와 거의 같다.)
 // axios를 쓰는 목적은 서버에 데이터를 요청할 때 비동기적으로 요청하려고
 
+
+
 function App() {
 
-  const selectAll = () =>{
-    alert('selectAll')
-    axios.get('/movies')
+  const [movie, setMovie] = useState([])
 
-    // 서버에 전송해서 응답d
+  const selectAll = async() =>{
+    const result = await axios.get('/movies')
+    console.log(result)
+    // http://localhost:3500/movies
+    // ES6 - template string, arrow function, async
+    // async 하고 await axios~ 하면 해당 데이터를 받을때 까지 기다린다.는 뜻
+    // 서버에서 보낸 정보를 받아와서 result에 넣고, 브라우저 console에 출력
+
+
+    // 서버에 전송해서 응답
     // package.json 에 proxy 설정해주면 앞의 경로를 생략가능
     // axios.get('/movies/')
+    setMovie([...result.data])
+    console.log(movie)
+  }
+
+  const selectWhere = async() => {
+    const result = await axios.get('/movies/'+3)
+    console.log(result)
+  }
+
+  const insertData = async() => {
+    alert('insertData')
+    const movieObj = {id:4 , title : '블랙 아담', director:'드웨인 존슨', running_time:130}
+    await axios.post('/movies',movieObj)
+    // post : 추가(create) 요청할 때 쓰는 메서드
+    // post는 뒤에 콤마 적고, 객체를 넣으면 알아서 넘어간다.
+  }
+
+  const updateData = async() => {
+    alert('updateData')
+    const updateObj = {id:3,director:'김하나', running_time:130}
+    const result = await axios.put('/movies',updateObj)
+  }
+
+  const deleteData = async() => {
+    alert('deleteData')
+    const deleteObj = {id:5, title:'dfd'}
+    const result = await axios.delete('/movies/'+5)
   }
 
   return (
     <div id="App">
       <h1>React-Express-Mysql Connect</h1>
       <button onClick={selectAll}>모두 조회</button>
+      <button onClick={selectWhere}>조건 조회</button>
+      <button onClick={insertData}>추가</button>
+      <button onClick={updateData}>수정</button>
+      <button onClick={deleteData}>삭제</button>
+      {movie.map((data)=>(
+        <div style={{width:'400px', height:'50px', backgroundColor:'pink', borderBottom:'1px solid gray'}} 
+        key={data.id}>{data.id} {data.title} {data.director} {data.running_time} </div>
+      ))}
     </div>
   );
 }
--- Front_React_Example/react-express-mysql-practice/src/server/server.js
+++ Front_React_Example/react-express-mysql-practice/src/server/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+// req.body를 나오게 하기위해 사용, 사용하지않으면 body가 undefined가 나온다.
+
 const db = require('./config/db.js')
 
 const PORT = process.env.PORT || 3500
@@ -11,11 +15,85 @@ app.get('/',(req,res)=>{
 
 app.get('/movies',(req,res)=>{
     console.log('/movies')
+    db.query("select * from movie",(err,data)=>{
+        if(!err){
+            console.log(data)
+            res.send(data)
+            // response, send
+            // 응답을 클라이언트쪽으로 보낸다. data가 react로 보내진다.
+        }
+        else{
+            console.log(err)
+        }
+    })
 })
 
-app.get('/',(req,res)=>{
+app.get('/movies/:id',(req,res)=>{
+    console.log('/movie/:id')
+    const id = req.params.id
+    console.log(id)
+    db.query(`select * from movie where id=${id}`,(err,data)=>{
+        if(!err){
+            console.log(data)
+            res.send(data)
+        }
+        else{
+            console.log(err)
+        }
+    })
+})
 
+app.post('/movies',(req,res)=>{
+    console.log('/movies(post)')
+    const target = req.body
+    const id = target.id
+    const title = target.title
+    const director = target.director
+    const run_time = target.running_time
+    console.log(req.body)
 
+    db.query(`insert into movie values(${id},'${title}','${director}',${run_time})`,(err,data)=>{
+        if(!err){
+            console.log('입력이 완료되었습니다.')
+        }
+        else{
+            console.log(err)
+        }
+    })
+    // get 으로 넘어온 정보는 req.params 지만 post로 넘어온 정보는 req.body에 있다.
+})
+
+app.put('/movies',(req,res)=>{
+    console.log('movies(update)')
+    console.log(req.body)
+    const target = req.body
+    const id = target.id
+    const director = target.director
+    const run_time = target.running_time
+
+    db.query(`update movie set director = '${director}',running_time=${run_time} where id=${id}`,(err,data)=>{
+        if(!err){
+            console.log('수정 완료!')
+        }
+        else{
+            console.log(err)
+        }
+    })
+})
+
+app.delete('/movies/:id',(req,res)=>{
+    console.log('movies(delete)')
+    console.log(req.body)
+    console.log(req.params.id)
+    const id = req.params.id
+    db.query(`delete from movie where id = ${id}`,(err,data)=>{
+        if(!err){
+            console.log('삭제 완료')
+        }
+        else{
+            console.log(err)
+        }
+    })
 })
 
 app.listen(PORT,()=>{
